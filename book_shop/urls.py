42fd3fe242ee3443673fd19e3613196dba63f682
Kate <bbkisik@gmail.com>
2021-01-28 11:04:38 +0300

    rollback

--- book_shop/urls.py
+++ book_shop/urls.py
@@ -17,17 +17,13 @@ import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('manager.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:  #
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# для отображения медиа на проде
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
